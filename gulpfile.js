c724cc1b299700698ae680fe0c021897ef3ae14e
Steele <kevin.steele@nycfhc-nmx3955.na.corp.ipgnetwork.com>
2020-08-07 15:37:51 -0400

    init

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require("gulp"); 
+const nunjucksRender = require('gulp-nunjucks-render');
+const data = require("gulp-data");
+const beautify = require("gulp-beautify");
+const removeEmptyLines = require("gulp-remove-empty-lines");
+const jeditor = require("gulp-json-editor");
+const rename = require("gulp-rename");
+
+
+function buildFromNJK() {
+  return gulp.src("src/pages/*")
+  .pipe(data(function() {
+    return require("./src/data/data.json");
+  }))
+  .pipe(nunjucksRender({
+    path: ['src/templates']
+  }))
+  .pipe(beautify.html({indent_size: 2}))
+  .pipe(removeEmptyLines())
+  .pipe(gulp.dest('dist'))
+}
+
+function editJSON() {
+  
+}
+
+
+
+exports.sampleSetup = series(buildFromNJK);
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Gulp NunJucks Boilerplate for SPAs or Microsites
+## Purpose
+This repo is used as a base for creating single-page-oriented experiences using **gulp** as a task runner/build tool and **nunjucks**. Below are some helpful reference links and blogs this was inspired by.
+
+## Links
+[text](https://link)
+[text](https://link)
+[text](https://link)
+[text](https://link)
